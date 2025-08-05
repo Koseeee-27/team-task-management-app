b6f8d68eb293bddcaefcbf60beba5a6836025c87
Koseeee-27 <kosekose2741@gmail.com>
2025-08-05 13:42:46 +0900

    /projectのpage.tsx用意

---
+++ frontend/app/project/page.tsx
@@ -0,0 +1,12 @@
+import ProjectWizard from '@/features/project/components/ProjectWizard'
+import React from 'react'
+
+const ProjectPage = () => {
+  return (
+    <div>
+      <ProjectWizard />
+    </div>
+  )
+}
+
+export default ProjectPage
